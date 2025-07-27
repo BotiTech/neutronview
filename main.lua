4e3617409feb39b156e1810a18068fdd064bb4d4
BotiTech <backupemailwuf@gmail.com>
2025-07-27 22:49:56 +0200

    Initialize rod addresses in main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ local workspace,window,menu = system.addWindow(gui.filledWindow(1,1,82,28,0x2626
 window:addChild(gui.label(window.width/2-20,1,40,3,0xFFFF00,"NeutronView v1.0")):setAlignment(gui.ALIGNMENT_HORIZONTAL_CENTER, gui.ALIGNMENT_VERTICAL_CENTER)
 -------
 
-local rod_addrs = {}
+local rod_addrs = {"ec2e625b", "e3c6b4a7", "e4d8c9b2", "e5f9d2c3"}
 
 local rods = gui.list(2,4,20,#rod_addrs*4,4,0,0x000000,0xFFFFFF,0x6D6D6D,0xFFFF00,0x000000,0xFFFF00,false)
 rods:addItem("Up")
