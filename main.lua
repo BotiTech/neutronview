78b925f8ff474463812457d7298f12a77dec3e96
BotiTech <backupemailwuf@gmail.com>
2025-07-27 22:55:28 +0200

    Fix rod list dimensions in main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ window:addChild(gui.label(window.width/2-20,1,40,3,0xFFFF00,"NeutronView v1.0"))
 
 local rod_addrs = {"ec2e625b", "e3c6b4a7", "e4d8c9b2", "e5f9d2c3"}
 
-local rods = gui.list(2,4,20,#rod_addrs*4,4,0,0x000000,0xFFFFFF,0x6D6D6D,0xFFFF00,0x000000,0xFFFF00,false)
+local rods = gui.list(2,4,20,#rod_addrs*2,2,0,0x000000,0xFFFFFF,0x000000,0xFFFF00,0x000000,0xFFFF00,false)
 rods:addItem("Up")
 rods:addItem("Center")
 rods:addItem("Down")
